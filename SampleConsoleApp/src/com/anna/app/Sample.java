656e1d99743c9cc07f58bb0ec6e8f8947f0d1b84
aryassasi <aryassasi@gmail.com>
2020-02-25 17:25:25 +0530

    second commit

--- SampleConsoleApp/src/com/anna/app/Sample.java
+++ SampleConsoleApp/src/com/anna/app/Sample.java
@@ -5,6 +5,9 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello .. Welcome to sample program");
+		
+		//By Arya
+		System.out.println("Hey..I got it");
 
 	}
 
