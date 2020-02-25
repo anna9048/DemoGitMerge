31a2b864f86a01e2b7fe8e49886234ecc41cafb1
aryassasi <aryassasi@gmail.com>
2020-02-25 18:17:52 +0530

    7th commit

--- SampleConsoleApp/src/com/anna/app/Sample.java
+++ SampleConsoleApp/src/com/anna/app/Sample.java
@@ -3,21 +3,17 @@ package com.anna.app;
 public class Sample {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello .. Welcome to sample program");
-		
-		//By Arya
+
+		// By Arya
 		System.out.println("Hey..I got it");
-		
-		
+
 		System.out.println("Happy to see u..");
 		System.out.println("Pls..Send me response");
 		System.out.println("Keep in touch");
-	
-		//By Anna
-		System.out.println("Yes ofcourse.");
-		System.out.println("got it..");
 
+		// End of program
 
 	}
 
